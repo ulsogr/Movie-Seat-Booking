00db7d80f0821cd220d2fbbab3794b7710c1a339
ulsogr <ulasoger@outlook.com>
2024-01-13 17:59:14 +0300

    using localStorage

--- Movie Seat Booking/script.js
+++ Movie Seat Booking/script.js
@@ -6,7 +6,13 @@ const movieSelect = document.getElementById("movie");
 
 let ticketPrice = movieSelect.value;
 
-const currentcount = parseInt(count.textContent);
+populateUI();
+
+//seçilen filmin koltuklaarını kaydetme
+function setMovieData(movieIndex, movieValue) {
+  localStorage.setItem("selectMovieIndex", movieIndex);
+  localStorage.setItem("selectMoviePrice", movieValue);
+}
 
 function selectseat(input) {
   if (
@@ -19,13 +25,35 @@ function selectseat(input) {
 
 function updatecount() {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
+
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+
   count.textContent = selectedSeats.length;
   total.textContent = selectedSeats.length * ticketPrice;
+}
+
+// localstorage veri alma ve kullanma
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    selectedSeats.forEach((seat) => {
+      seats[seat].classList.toggle("selected");
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem("selectMovieIndex");
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
 }
 
 //film seçimi
 movieSelect.addEventListener("change", (e) => {
   ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
 });
 
 //koltuk seçimi
@@ -33,3 +61,5 @@ container.addEventListener("click", (e) => {
   selectseat(e);
   updatecount();
 });
+
+updatecount(); //sayfa yenilendiğinde yüklemek için.
